7c6c93028863e6d3dcf18b3e8c81f93bf242ad19
Nikolay Lyukshin <kolya.net1111@mail.ru>
2023-05-16 19:32:11 +0300

    finish

--- server/index.js
+++ server/index.js
@@ -1,9 +1,13 @@
 const express = require('express');
 require('dotenv').config();
 const router = require('./routers');
+const { db } = require('./db');
 
 const app = express();
+
 const PORT = process.env.PORT
+
+db.migrate.latest();
 
 app.use(express.json());
 app.use(router);
